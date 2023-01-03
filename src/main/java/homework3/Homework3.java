cc0eaf8db1247c2f59c9459bbd7e3b173c9f4787
Атанас Туманов <atanastumanov@gmail.com>
2023-01-03 14:37:51 +0200

    Initial commit

---
+++ src/main/java/homework3/Homework3.java
@@ -0,0 +1,29 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+
+        //task 1
+        int a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number a:");
+        a = scanner.nextInt();
+
+        System.out.println("Enter number b:");
+        b = scanner.nextInt();
+
+        System.out.println("Enter number c:");
+        c = scanner.nextInt();
+
+        if (a>b && b>c) ;
+        System.println("The Largest number is: %d");
+
+        else if (b>c);
+        System.println("The largest number is: %d");
+
+        else
+            System.printl("The largest number is: %d");
+    }
+}
