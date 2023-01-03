46ad01d7a2eed7af1c1c3f07b5c2cd0ec37bf49e
Атанас Туманов <atanastumanov@gmail.com>
2023-01-03 15:24:44 +0200

    Homework3

--- src/main/java/homework3/Homework3.java
+++ src/main/java/homework3/Homework3.java
@@ -5,25 +5,31 @@ import java.util.Scanner;
 public class Homework3 {
     public static void main(String[] args) {
 
-        //task 1
+       //task 1
+
         int a, b, c;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number a:");
+        System.out.println("Enter the first number:");
         a = scanner.nextInt();
-
-        System.out.println("Enter number b:");
+        System.out.println("Enter the second number:");
         b = scanner.nextInt();
-
-        System.out.println("Enter number c:");
+        System.out.println("Enter the third number:");
         c = scanner.nextInt();
-
-        if (a>b && b>c) ;
-        System.println("The Largest number is: %d");
-
-        else if (b>c);
-        System.println("The largest number is: %d");
-
+        if(a > b && b > c)
+        {
+            System.out.println("Largest number is: " + a);
+        }
+        else if(b > c)
+        {
+            System.out.println("Largest number is: " + b);
+        }
         else
-            System.printl("The largest number is: %d");
+        {
+            System.out.println("Largest number is: " + c);
+        }
+
     }
 }
+
+
+
